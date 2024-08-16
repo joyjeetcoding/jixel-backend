8a97c1153a44367abf59db185b4a330137f81eab
joyjeetcoding <joyxbhp6@gmail.com>
2024-08-16 18:22:28 +0530

    Some changes

--- controller/post.controller.ts
+++ controller/post.controller.ts
@@ -1,48 +1,49 @@
 import cloudinary from "cloudinary";
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 import PostModel from "../models/posts.model";
-export const createPost = async(req: Request, res: Response) => {
-    try {
-        const {title, summary, description} = req.body;
 
-        const image = req.file as Express.Multer.File;
+export const createPost = async (req: Request, res: Response) => {
+  try {
+    const { title, summary, description } = req.body;
 
-        let imageUrl;
+    const image = req.file as Express.Multer.File;
 
-        if(image) {
-            const base64Image = Buffer.from(image.buffer).toString("base64");
+    let imageUrl;
+    if (image) {
+      const base64Image = Buffer.from(image.buffer).toString("base64");
 
-            const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+      const dataURI = `data:${image.mimetype};base64,${base64Image}`;
 
-            const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
-            
-            imageUrl = uploadResponse.secure_url;
-        }
+      const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
 
-        const newPost = new PostModel({
-            title,
-            summary,
-            description,
-            imgUrl: imageUrl,
-        });
+      imageUrl = uploadResponse.secure_url;
+    }
 
-        if(newPost) {
-            await newPost.save();
+    const newPost = new PostModel({
+      title,
+      summary,
+      description,
+      imgUrl: imageUrl,
+    });
 
-            res.status(200).json({
-                title: newPost.title,
-                summary: newPost.summary,
-                imgUrl: newPost.imgUrl,
-                author: newPost.author
-            })
-        } else {
-            res.status(400).json("Invalid User Post")
-        }
-    } catch (error:any) {
-        console.log("Error in createPost Controller", error.message);
-        
-        res.status(500).json({
-            error: "Internal Server Error"
-        })        
-   }
-}
+    if (newPost) {
+      await newPost.save();
+
+      res.status(200).json({
+        title: newPost.title,
+        summary: newPost.summary,
+        description: newPost.description,
+        imgUrl: newPost.imgUrl,
+        author: newPost.author,
+      });
+    } else {
+      res.status(400).json("Invalid User Post");
+    }
+  } catch (error: any) {
+    console.log("Error in createPost Controller", error.message);
+
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
