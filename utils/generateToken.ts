7023c56eadfbe3d54725410016df26933ee8aeb2
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-08-30 00:08:01 +0530

    GenerateToken node_env changed

--- utils/generateToken.ts
+++ utils/generateToken.ts
@@ -3,14 +3,14 @@ import { Response } from "express";
 
 const generatetokenandSetCookie = (userId: Object, res: Response) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
-    expiresIn: "15d",
+    expiresIn: "15d", // Token expires in 15 days
   });
 
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 100,
-    httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
+    httpOnly: true, // Cookie cannot be accessed via JavaScript
+    sameSite: "strict", // Ensure this is appropriate for your use case
+    secure: process.env.NODE_ENV === "production", // Ensure cookies are only sent over HTTPS in production
   });
 };
 
